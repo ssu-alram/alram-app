f1ce23ba308cc6e1d51d8a9afd4e54e0d97c1421
yuunseo <dbstj7457@naver.com>
2022-11-09 10:48:07 +0900

    알람화면 만들기시작

---
+++ app/src/main/java/com/example/alarm/MainAlarm.java
@@ -0,0 +1,14 @@
+package com.example.alarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainAlarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_alarm);
+    }
+}
