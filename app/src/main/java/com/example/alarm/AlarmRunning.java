7a652328e2db9190852bea0b15c04191de5d58b2
yuunseo <dbstj7457@naver.com>
2022-11-13 13:03:42 +0900

    알람 백그라운드 실행화면 작성

---
+++ app/src/main/java/com/example/alarm/AlarmRunning.java
@@ -0,0 +1,14 @@
+package com.example.alarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlarmRunning extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alarm_running);
+    }
+}
