f0c9d9684b0ee6f71d8f6be1a25bc804adea718a
yuunseo <dbstj7457@naver.com>
2022-11-28 17:24:43 +0900

    프래그머늩 하는중 환경설정,,

--- app/src/main/java/com/example/alarm/SettingFragment.java
+++ app/src/main/java/com/example/alarm/SettingFragment.java
@@ -2,30 +2,40 @@ package com.example.alarm;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
+import android.util.Log;
 
-import androidx.annotation.Nullable;
+import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 
-public class SettingFragment extends PreferenceFragment {
-
-    SharedPreferences pref;
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.root_preferences);
-    }
+public class SettingFragment extends PreferenceFragmentCompat {
+    SharedPreferences prefs;
 
     @Override
-    public void onResume(){
-        super.onResume();
-        SharedPreferences.OnSharedPreferenceChangeListener listner;
-        pref.registerOnSharedPreferenceChangeListener(listner);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.settings_preferences);
+        prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        prefs.registerOnSharedPreferenceChangeListener(prefListener);
     }
-    @Override
-    public void onPause(){
-        super.onPause();
-        pref.unregisterOnSharedPreferenceChangeListener(listner);
-    }
+
+    SharedPreferences.OnSharedPreferenceChangeListener prefListener =
+            new SharedPreferences.OnSharedPreferenceChangeListener() {
+                @Override
+                public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                    // key값에 해당하는 명령 넣기
+                    if (key.equals("soundset")) {
+                        Log.d("TAG", key + "SELECTED");
+                    } else if (key.equals("vibrate")) {
+                        Log.d("TAG", key + "SELECTED");
+                    } else if (key.equals("sound_list")) {
+                        Log.d("TAG", key + "SELECTED");
+                    } else if (key.equals("defaultset")) {
+                        Log.d("TAG", key + "SELECTED");
+                    } else if (key.equals("push_alarm")) {
+                        Log.d("TAG", key + "SELECTED");
+                    }
+
+                }
+            };
+
 }
 
--- app/src/main/java/com/example/alarm/SettingsActivity.java
+++ app/src/main/java/com/example/alarm/SettingsActivity.java
@@ -2,7 +2,6 @@ package com.example.alarm;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
@@ -17,8 +16,8 @@ public class SettingsActivity extends AppCompatActivity {
     class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey);
-            addPreferencesFromResource(R.xml.root_preferences);
+            setPreferencesFromResource(R.xml.settings_preferences, rootKey);
+            addPreferencesFromResource(R.xml.settings_preferences);
         }
     }
 }}
