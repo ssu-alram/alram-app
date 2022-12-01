b146cf542f79cebcd88a7e4681b046b34718dcde
yuunseo <dbstj7457@naver.com>
2022-12-01 10:38:13 +0900

    "타임피커된다!"

--- app/src/main/java/com/example/alarm/SettingsFragment.java
+++ app/src/main/java/com/example/alarm/SettingsFragment.java
@@ -3,8 +3,11 @@ package com.example.alarm;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
@@ -12,10 +15,42 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     SharedPreferences spref ;
     SharedPreferences.Editor editor;
 
+    SharedPreferences.OnSharedPreferenceChangeListener listner = new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            if(key.equals("진동")){
+                boolean b = spref.getBoolean("진동",false);
+                Toast.makeText(getActivity(), "진동 알림 :" +b, Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
     PreferenceScreen sound;
     PreferenceScreen simple;
     PreferenceScreen language;
     PreferenceScreen display;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.root_preferences);
+
+        spref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        boolean isSound = spref.getBoolean("sound",false);
+        Toast.makeText(getActivity(), "진동"+isSound, Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        spref.registerOnSharedPreferenceChangeListener(listner);
+
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        spref.unregisterOnSharedPreferenceChangeListener(listner);
+    }
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
